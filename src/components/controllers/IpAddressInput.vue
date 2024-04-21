35cbcdc34124057e318ba6299abc16b362301e90
Ruslan <ruslan.web.time@gmail.com>
2024-04-22 01:25:21 +0500

    fix modal responsive

--- src/components/controllers/IpAddressInput.vue
+++ src/components/controllers/IpAddressInput.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="card bg-base-100 shadow-xl p-4 my-2">
     <div class="font-bold indent-2 dark:text-white">IP - адрес</div>
-    <div class="flex gap-2 flex-wrap items-center">
+    <div class="flex sm:gap-2 gap-1 flex-wrap items-center">
       <UiInput
           name="address-first"
-          inputClass="input-sm w-20"
+          inputClass="input-sm sm:w-20 w-[65px]"
           v-model="networkStart"
-      /> .
+      />.
       <UiInput
           name="address-second"
-          inputClass="input-sm w-20"
+          inputClass="input-sm sm:w-20 w-[65px]"
           v-model="networkEnd"
-      /> .
+      />.
       <UiInput
           name="address-third"
-          inputClass="input-sm w-20"
+          inputClass="input-sm sm:w-20 w-[65px]"
           value="1"
           v-model="subnet"
-      /> .
+      />.
       <UiInput
           name="address-fourth"
-          inputClass="input-sm w-20"
+          inputClass="input-sm sm:w-20 w-16"
           v-model="computer"
           disabled
       />
